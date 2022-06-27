ad75d2b6f343cf451a498fc5186d811ce3d2b285
omouravictor <omouravictor@gmail.com>
2022-06-27 20:33:43 -0300

    CurrencyAPI interface created

---
+++ app/src/main/java/com/omouravictor/currencynow/data/CurrencyApi.kt
@@ -0,0 +1,16 @@
+package com.omouravictor.currencynow.data
+
+import com.omouravictor.currencynow.data.models.CurrencyResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CurrencyApi {
+
+    @GET("/exchangerates_data/latest")
+    suspend fun getRates(
+        @Query("base") fromCurrency: String,
+        @Query("symbols") toCurrencies: String,
+        @Query("apikey") apikey: String
+    ): Response<CurrencyResponse>
+}
