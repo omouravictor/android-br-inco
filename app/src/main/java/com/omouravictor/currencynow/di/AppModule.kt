dfcb4ce8465b0f31398e9361ec30feea59532660
omouravictor <omouravictor@gmail.com>
2022-06-27 20:34:17 -0300

    AppModule created

---
+++ app/src/main/java/com/omouravictor/currencynow/di/AppModule.kt
@@ -0,0 +1,47 @@
+package com.omouravictor.currencynow.di
+
+import com.omouravictor.currencynow.data.CurrencyApi
+import com.omouravictor.currencynow.main.DefaultMainRepository
+import com.omouravictor.currencynow.main.MainRepository
+import com.omouravictor.currencynow.util.DispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://api.apilayer.com/"
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCurrencyApi(): CurrencyApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CurrencyApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(api: CurrencyApi): MainRepository = DefaultMainRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
+}
